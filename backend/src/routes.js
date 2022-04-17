9eb31df6d10d0e29c459cb587f3f50d5d96163dd
Lucas-Wilian <lucaswilianr.s@gmail.com>
2022-04-17 19:48:05 -0300

    Segundo commit: Finalização do backend �💯🔥

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 const AnnotationsController = require('./controllers/AnnotationController')
+const PriorityController = require('./controllers/PriorityController')
+const ContentController = require('./controllers/ContentController')
 
 // Rotas para anotaçoes
 routes.post('/annotations', AnnotationsController.create)
 routes.get('/annotations', AnnotationsController.read)
 routes.delete('/annotations/:id', AnnotationsController.delete )
+
+// Rota para prioridade
+routes.get('/priorities', PriorityController.read )
+routes.post('/priorities/:id', PriorityController.update)
+
+// Rota Update
+routes.post('/contents/:id', ContentController.update)
+
 module.exports = routes
